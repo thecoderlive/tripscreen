5153397cc8ab07e4da21138dffe5a2e703f12e86
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-01-25 13:24:50 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,81 @@
+export const item = {
+    "id": "b3fe4ebd-e171-495e-91a3-36962afc2100",
+    "rating_type": "event",
+    "rating_count": 8843,
+    "rating_type2": "total",
+    "rating_count2": 6083,
+    "rating_type3": "road",
+    "rating_count3": 7104,
+    "rating_type4": "yes",
+    "rating_count4": 9827,
+    "mentions_heading": "fear",
+    "mentions_list": [
+        {
+            "id": "f1305fd7-5e41-402c-8615-139ff03d6cc8",
+            "mention_title": "eight",
+            "review_count": "strong"
+        },
+        {
+            "id": "f4d7503c-52d3-4db2-a896-570f3df04684",
+            "mention_title": "answer",
+            "review_count": "management"
+        },
+        {
+            "id": "faf15df5-1f54-485f-bcbd-a333cd47e66f",
+            "mention_title": "least",
+            "review_count": "itself"
+        },
+        {
+            "id": "73d6d2e0-5607-4f71-ba2b-be3e8a4db878",
+            "mention_title": "game",
+            "review_count": "real"
+        },
+        {
+            "id": "a9a18759-de20-4ead-8e84-b3c0090075ec",
+            "mention_title": "moment",
+            "review_count": "step"
+        }
+    ],
+    "review_list": [
+        {
+            "id": "cc0c20ac-b4cc-4d2e-8c84-ad507bdffd2b",
+            "user_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/115.png",
+            "date": "tough",
+            "review_title": "color",
+            "review": "Expert side evening race. Let with wrong response program professional.",
+            "visited_dates": "never"
+        },
+        {
+            "id": "40b5f519-e08c-4c59-92f0-196419826103",
+            "user_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/289.png",
+            "date": "opportunity",
+            "review_title": "evidence",
+            "review": "Factor right go high. Stop police charge there air though list.",
+            "visited_dates": "various"
+        },
+        {
+            "id": "a6ca1737-4256-4456-8f42-922d56f11717",
+            "user_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/355.png",
+            "date": "act",
+            "review_title": "along",
+            "review": "Size figure possible act claim occur. I music him mission. Become value your so listen suggest everyone program.",
+            "visited_dates": "information"
+        },
+        {
+            "id": "01fa620d-d180-4aad-a5dc-14609fe13826",
+            "user_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/212.png",
+            "date": "child",
+            "review_title": "serve",
+            "review": "Company cultural what artist child buy whose. Two in push force yard any. Condition produce benefit turn laugh suggest.",
+            "visited_dates": "south"
+        },
+        {
+            "id": "120966eb-50e0-45d8-a83c-295f3e086bcf",
+            "user_pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/297.png",
+            "date": "store",
+            "review_title": "suggest",
+            "review": "Weight establish when week word and product. Event sign end against help community.",
+            "visited_dates": "scientist"
+        }
+    ]
+}
